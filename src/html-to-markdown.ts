eead45db9f7c5a28d3b25c28aeac8df32406c85c
Dup4 <lyuzhi.pan@gmail.com>
2022-02-09 13:06:40 +0800

    fix: remove node-html-md

--- src/html-to-markdown.ts
+++
@@ -1,9 +0,0 @@
-import { NodeHtmlMarkdown } from "node-html-markdown";
-
-const Html2Md = new NodeHtmlMarkdown(
-    { keepDataImages: true },
-    undefined,
-    undefined
-);
-
-export default Html2Md;
