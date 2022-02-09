e81bbbdfb83500d43fbbf159a5910b0b2140091f
Dup4 <lyuzhi.pan@gmail.com>
2022-02-09 13:24:05 +0800

    fix: rename func

--- src/contest.ts
+++ src/contest.ts
@@ -10,23 +10,23 @@ export async function New(slug: string, dst: string) {
     dst = path.join(dst, String(contest.contest?.id));
     fs.mkdirSync(dst, { recursive: true });
 
-    DownloadStatement(problems, dst);
+    DownloadProblem(problems, dst);
 }
 
 export async function Pull(slug: string, dst: string) {
     const contest = await Contest.build(slug);
     const problems = await contest.getProblems();
 
-    DownloadStatement(problems, dst);
+    DownloadProblem(problems, dst);
 }
 
-async function DownloadStatement(problems: Array<Problem>, dst: string) {
+async function DownloadProblem(problems: Array<Problem>, dst: string) {
     for (const ix in problems) {
         const pId = String.fromCharCode("a".charCodeAt(0) + Number(ix));
         const p = problems[ix];
 
         const targetDir = path.join(dst, pId);
 
-        ProblemUtils.DownloadStatement(p, targetDir);
+        ProblemUtils.DownloadProblem(p, targetDir);
     }
 }
